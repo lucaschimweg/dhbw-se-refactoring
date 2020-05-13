67f5e5a1e13c8fc2f18bc1aefb1ac027a4ebde61
lucaschimweg <l.schimweg@gmail.com>
2020-05-13 15:36:29 +0200

    Move Function
    https://refactoring.com/catalog/moveFunction.html

--- src/Customer.java
+++ src/Customer.java
@@ -28,7 +28,7 @@ class Customer {
         for (Rental each : rentals) {
             double thisAmount = 0;
             //determine amounts for each line
-            thisAmount = amountFor(each);
+            thisAmount = each.getAmount();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -43,26 +43,6 @@ class Customer {
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
         return result.toString();
-    }
-
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
     }
 
 }

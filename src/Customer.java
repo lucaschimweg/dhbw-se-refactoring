d2598827bf27168a5d5ebe05855217351b126b49
lucaschimweg <l.schimweg@gmail.com>
2020-05-13 15:52:10 +0200

    Replace Function with Command
    https://refactoring.com/catalog/replaceFunctionWithCommand.html

--- src/Customer.java
+++ src/Customer.java
@@ -4,10 +4,15 @@ import java.util.*;
 
 class Customer {
     private String name;
+
     private final Vector<Rental> rentals = new Vector<>();
 
     public Customer(String newname) {
         name = newname;
+    }
+
+    public Vector<Rental> getRentals() {
+        return rentals;
     }
 
     public void addRental(Rental arg) {
@@ -19,30 +24,7 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration<Rental> enum_rentals = rentals.elements();
-        StringBuilder result = new StringBuilder("Rental Record for " + this.getName() + "\n");
-        result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
-
-        for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            thisAmount = each.getAmount();
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-            //show figures for this rental
-            result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(thisAmount).append("\n");
-            totalAmount += thisAmount;
-        }
-
-        //add footer lines
-        result.append("Amount owed is ").append(totalAmount).append("\n");
-        result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
-        return result.toString();
+        return new Statement(this).toString();
     }
 
 }
---
+++ src/Statement.java
@@ -0,0 +1,38 @@
+import java.util.Enumeration;
+
+public class Statement {
+
+    private final Customer customer;
+
+    public Statement(Customer cust) {
+        this.customer = cust;
+    }
+
+    public int getFrequentRenterPoints() {
+        return customer.getRentals().stream().parallel().mapToInt(rental -> {
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) {
+                return 2;
+            }
+            return 1;
+        }).sum();
+    }
+
+    public double getTotalAmount() {
+        return customer.getRentals().stream().parallel().mapToDouble(Rental::getAmount).sum();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("Rental Record for " + customer.getName() + "\n");
+        result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
+
+        for (Rental each : customer.getRentals()) {
+            result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(each.getAmount()).append("\n");
+        }
+
+        //add footer lines
+        result.append("Amount owed is ").append(getTotalAmount()).append("\n");
+        result.append("You earned ").append(getFrequentRenterPoints()).append(" frequent renter points");
+        return result.toString();
+    }
+}
